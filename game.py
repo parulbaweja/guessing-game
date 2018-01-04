9b593dbf7ad92b87cadf2b819d78b34736d099ac
Hackbright Student <no-reply@hackbrightacademy.com>
2018-01-03 16:33:54 -0800

    Confirming Integer Status

--- game.py
+++ game.py
@@ -6,16 +6,9 @@ print "Howdy, what's your name?"
 name = raw_input("(type in your name)")
 print "{name}, I'm thinking of a number between 1 and 100".format(name=name)
 # print "{}, I'm thinking of a number between 1 and 100".format(name)
-print "Try to guess my number"
 
-num = random.randint(1, 100)
-print num
 
-num_guesses = 0
-
-while True:
-    guess = raw_input("Your guess?")
-    
+def is_valid(guess):
 
     try:
         guess = int(guess)
@@ -23,19 +16,46 @@ while True:
         print "That's not a number! Try again with a valid number."
         continue
 
-    
     if guess < 1 or guess > 100:
         print "That's not between 1 and 100! Try again with a valid number."
         continue
 
-    num_guesses += 1
+def play_once():
 
-    if guess > num:
-        print "Your guess was too high, try again."
-    elif guess < num:
-        print "Your guess was too low, try again."
-    else:
-        response = "Well, done, {name}! You found my number in {n} tries!"
-        print response.format(name=name, n=num_guesses)
-        # response = "Well, done, {}! You found my number in {} tries!".format(name, num_guesses)
-        break
+    print "Try to guess my number"
+
+    num = random.randint(1, 100)
+    print num
+
+    num_guesses = 0
+
+
+    while True:
+        guess = raw_input("Your guess?")
+
+        is_valid(guess)
+
+        num_guesses += 1
+
+        if guess > num:
+            print "Your guess was too high, try again."
+        elif guess < num:
+            print "Your guess was too low, try again."
+        else:
+            response = "Well, done, {name}! You found my number in {n} tries!"
+            print response.format(name=name, n=num_guesses)
+            # response = "Well, done, {}! You found my number in {} tries!".format(name, num_guesses)
+            decision = raw_input("Would you like to play again? (y/n)")
+            break
+
+def play_again():
+
+    play_once()
+    least_guesses = num_guesses
+    
+    while decision == "y":
+        if num_guesses < least_guesses:
+            least_guesses = num_guesses
+        play_once()
+        
+play_again()
